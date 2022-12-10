545624eca77782c9c39fa023b239a07a0055fe5c
AlexRuMath <bioshock20129@gmail.com>
2022-12-11 00:22:17 +0300

    ert5rergh

--- app/deploy-dev.sh
+++ app/deploy-dev.sh
@@ -3,10 +3,4 @@
 name="app-dev"
 echo $name
 
-if ! [[ $(docker ps -a --filter="name=$CONTAINER_NAME" --filter "status=exited" | grep -w "$CONTAINER_NAME") ]] then
-then
-    docker run --publish 3000:3000 --name $name alexrumath/app
-else
-    docker stop $name
-    docker run --publish 3000:3000 --name $name alexrumath/app
-fi
+docker run --rm --publish 3000:3000 --name $name alexrumath/app
--- app/deploy-prod.sh
+++ app/deploy-prod.sh
@@ -3,10 +3,4 @@
 name="app-prod"
 echo $name
 
-if ! docker ps --format '{{.Names}}' | grep -w $name &> /dev/null; 
-then
-    docker run --publish 3001:3000 --name $name alexrumath/app
-else
-    docker stop $name
-    docker run --publish 3001:3000 --name $name alexrumath/app
-fi
+docker run --rm --publish 3001:3000 --name $name alexrumath/app
