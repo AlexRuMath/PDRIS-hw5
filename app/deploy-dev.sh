b206468e844a0f06033052f2e38d4a54406a2d79
AlexRuMath <bioshock20129@gmail.com>
2022-12-11 00:16:43 +0300

    update deplot scripts

--- app/deploy-dev.sh
+++ app/deploy-dev.sh
@@ -4,5 +4,9 @@ name="app-dev"
 echo $name
 
 if ! docker ps --format '{{.Names}}' | grep -w $name &> /dev/null; then
-    docker run --publish 3000:3000 --name $name  alexrumath/app 
+then
+    docker run --publish 3000:3000 --name $name alexrumath/app
+else
+    docker stop $name
+    docker run --publish 3000:3000 --name $name alexrumath/app
 fi
--- app/deploy-prod.sh
+++ app/deploy-prod.sh
@@ -3,6 +3,10 @@
 name="app-prod"
 echo $name
 
-if ! docker ps --format '{{.Names}}' | grep -w $name &> /dev/null; then
-    docker run --publish 3001:3000 --name $name  alexrumath/app 
+if ! docker ps --format '{{.Names}}' | grep -w $name &> /dev/null; 
+then
+    docker run --publish 3001:3000 --name $name alexrumath/app
+else
+    docker stop $name
+    docker run --publish 3001:3000 --name $name alexrumath/app
 fi
