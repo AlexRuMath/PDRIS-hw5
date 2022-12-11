af5e6431740b437f39cf1cf3ff8d4a43fab2790a
AlexRuMath <bioshock20129@gmail.com>
2022-12-11 09:57:03 +0300

    update: detach

--- app/deploy-dev.sh
+++ app/deploy-dev.sh
@@ -4,4 +4,4 @@ name="app-dev"
 echo $name
 
 docker stop $name || true && docker rm $name || true
-docker run --rm --publish 3000:3000 --name $name alexrumath/app
+docker run -d --rm --publish 3000:3000 --name $name alexrumath/app
